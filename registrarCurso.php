b82e11b412c34b453448bded260670d1349390fc
JU4NG1R4L <juanj8845@gmail.com>
2019-05-10 23:17:02 -0500

    Se realizaron cambios

---
+++ registrarCurso.php
@@ -0,0 +1,17 @@
+<?PHP
+include 'conexion.php';
+
+$idcurso=$_POST["IdCurso"];
+$nombrecurso=$_POST["NombreCurso"];
+$añoencurso=$_POST["AñoEnCurso"];
+$materia_idmateria=$_POST["Materia_IdMateria"];
+$docente_iddocente=$_POST["Docente_IdDocente"];
+
+	$agregar = oci_parse($conexion,"INSERT INTO Curso VALUES ('$idcurso','$nombrecurso','$añoencurso', '$materia_idmateria', '$docente_iddocente')");
+
+	 if(oci_execute($agregar)){
+		echo "registra";
+	}else{
+		echo "noRegistra";
+	} 
+?>
