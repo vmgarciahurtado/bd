946215ca57261cc4343ca36d7d4796a6253f30df
JU4NG1R4L <juanj8845@gmail.com>
2019-05-15 17:52:48 -0500

    Se realizaron PHP

---
+++ registrarAgendaDocente.php
@@ -0,0 +1,19 @@
+<?PHP
+include 'conexion.php';
+
+    $IdAgenda= $_POST["IdAgenda"];
+    $Dia= $_POST["Dia"];
+    $HoraInicioClase= $_POST["HoraInicioClase"];
+    $HoraFinClase=$_POST["HoraFinClase"];
+    $HoraInicioAsesoria= $_POST["HoraInicioAsesoria"];
+    $HoraFinAsesoria= $_POST["HoraFinAsesoria"];
+    $Docente_IdDocente=$_POST["Docente_IdDocente"]
+    
+	$agregar = oci_parse($conexion,"INSERT INTO Agenda VALUES ('$IdAgenda', '$Dia', 
+    '$HoraInicioClase', '$HoraFinClase', '$HoraInicioAsesoria', '$HoraFinAsesoria', '$Docente_IdDocente')");
+	 if(oci_execute($agregar)){
+		echo "registra";
+	}else{
+		echo "noRegistra";
+	} 
+?>
