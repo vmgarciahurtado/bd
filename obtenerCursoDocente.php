1cd96c28315eadab1f9c1d720ee0b5dcbe0cd105
JU4NG1R4L <juanj8845@gmail.com>
2019-05-19 16:38:13 -0500

    SE REALIZO PHP

---
+++ obtenerCursoDocente.php
@@ -0,0 +1,21 @@
+<?PHP
+include 'conexion.php';
+
+//$docente=$_POST["docente_iddocente"];
+$json=array();
+$docente= "1024";
+
+$query = "SELECT nombrecurso
+FROM curso where docente_iddocente='$docente'";
+
+$statement = oci_parse ($conexion, $query);
+oci_execute ($statement);
+
+
+while ($row = oci_fetch_array ($statement, (OCI_NUM+OCI_RETURN_LOBS))) {
+	$result["nombreCurso"]= $row[0];
+	$json['cursos'][]=$result;
+}
+echo json_encode($json)
+
+?>
