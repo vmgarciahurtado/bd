2b78ce010cc5ba3c943435fe8605779899a1eef4
JU4NG1R4L <juanj8845@gmail.com>
2019-05-19 16:16:41 -0500

    CAMBIOS

--- consultarInasistencias.php
+++ consultarInasistencias.php
@@ -3,9 +3,10 @@ include 'conexion.php';
 
 $CodigoEstudiante=$_POST["CodigoEstudiante"];
 
-    $consultar = oci_parse($conexion,"SELECT c.NombreCurso, ec.fallasEstudiante
-    FROM Curso c JOIN Estudiantes_Curso ec ON(ec.Curso_IdCurso=c.IdCurso) JOIN Estudiante e ON
-    (ec.IdEstudiante_Curso=e.CodigoEstudiante) WHERE ec.Estudiante_CodigoEstudiante=$CodigoEstudiante");
+    $consultar = oci_parse($conexion,"SELECT c.NombreCurso, sc.inasistencias
+	FROM seguimiento_corte sc JOIN seguimiento s ON(sc.idseguimientocorte = s.seg_corte_idsegcorte)
+	JOIN curso c ON(c.idcurso=s.curso_idcurso) WHERE s.estudiante_codigoestudiante='$CodigoEstudiante'");
+	 
 	 if(oci_execute($consultar)){
 		echo "consulto";
 	}else{
