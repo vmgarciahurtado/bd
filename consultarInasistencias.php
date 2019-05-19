c847e13beca113def8814b7e4dbe351b5a584667
JU4NG1R4L <juanj8845@gmail.com>
2019-05-19 16:28:55 -0500

    SE REALIZARON CAMBIOS

--- consultarInasistencias.php
+++ consultarInasistencias.php
@@ -7,10 +7,14 @@ $CodigoEstudiante=$_POST["CodigoEstudiante"];
 	FROM seguimiento_corte sc JOIN seguimiento s ON(sc.idseguimientocorte = s.seg_corte_idsegcorte)
 	JOIN curso c ON(c.idcurso=s.curso_idcurso) WHERE s.estudiante_codigoestudiante='$CodigoEstudiante'");
 	 
-	 if(oci_execute($consultar)){
-		echo "consulto";
-	}else{
-		echo "noConsulto";
-	} 
+	 $statement = oci_parse ($conexion, $consultar);
+	 oci_execute ($statement);
+	 	 
+	 while ($row = oci_fetch_array ($statement, (OCI_NUM+OCI_RETURN_LOBS))) {
+		 $result["nombreCurso"]= $row[0];
+		 $result["inasistencias"]= $row[1];
+		 $json['fallas'][]=$result;
+	 }
+	 echo json_encode($json)
 ?>
 
--- consultarNotas.php
+++ consultarNotas.php
@@ -9,7 +9,7 @@ $CodigoEstudiante= "12345";
 $query = "SELECT sc.Corte_IdCorte, sc.DefinitivaCorte, e.nombreestudiante, c.nombrecurso
 FROM seguimiento_corte sc JOIN seguimiento s ON (sc.idseguimientocorte=s.seg_corte_idsegcorte)
 JOIN estudiante e ON(e.codigoestudiante=s.estudiante_codigoestudiante)
-JOIN curso c ON(s.curso_idcurso=c.idcurso) WHERE s.estudiante_codigoestudiante='$CodigoEstudiante';
+JOIN curso c ON(s.curso_idcurso=c.idcurso) WHERE s.estudiante_codigoestudiante='$CodigoEstudiante'";
 
 $statement = oci_parse ($conexion, $query);
 oci_execute ($statement);
