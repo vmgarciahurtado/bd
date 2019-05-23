7c41a1019b052d4211f0fa5f05f4b1d0af307712
JU4NG1R4L <juanj8845@gmail.com>
2019-05-23 16:27:45 -0500

    Se modificaron PHP

--- consultarNotas.php
+++ consultarNotas.php
@@ -6,10 +6,10 @@ include 'conexion.php';
 $json=array();
 $CodigoEstudiante= "12345";
 
-$query = "SELECT sc.Corte_IdCorte, sc.DefinitivaCorte, e.nombreestudiante, c.nombrecurso
-FROM seguimiento_corte sc JOIN seguimiento s ON (sc.idseguimientocorte=s.seg_corte_idsegcorte)
-JOIN estudiante e ON(e.codigoestudiante=s.estudiante_codigoestudiante)
-JOIN curso c ON(s.curso_idcurso=c.idcurso) WHERE s.estudiante_codigoestudiante='$CodigoEstudiante'";
+$query = "SELECT sc.corte_idcorte, sc.definitivacorte, c.nombrecurso, m.nombremateria
+FROM seguimiento_corte sc JOIN seguimiento s ON(sc.idseguimientocorte=s.seg_corte_idsegcorte)
+JOIN curso c ON(s.curso_idcurso=c.idcurso)
+JOIN materia m ON(m.idmateria=c.materia_idmateria) WHERE s.estudiante_codigoestudiante='$CodigoEstudiante'";
 
 $statement = oci_parse ($conexion, $query);
 oci_execute ($statement);
@@ -17,9 +17,9 @@ oci_execute ($statement);
 
 while ($row = oci_fetch_array ($statement, (OCI_NUM+OCI_RETURN_LOBS))) {
 	$result["corte"]= $row[0];
-	$result["definitiva"]= $row[1];
-	$result["nombre"]= $row[2];
-	$result["nombrecurso"]=$row[3];
+	$result["definitiva"]=$row[1];
+	$result["curso"]=$row[2];
+	$result["materia"]=$row[3];
 	$json['notas'][]=$result;
 }
 echo json_encode($json)
