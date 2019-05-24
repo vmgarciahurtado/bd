6547753a506173d6d47d14ed7248d6ce9df15282
vmgarciahurtado <vmgarciahurtado@gmail.com>
2019-05-24 14:38:50 -0500

    Cambios

--- estadoEstudiante.php
+++ estadoEstudiante.php
@@ -3,7 +3,6 @@ include 'conexion.php';
     $codigo= $_POST["codigoestudiante"];
     $estado= $_POST["estadoestudiante"];
     
-
     $agregar = oci_parse($conexion,"UPDATE estudiante SET estadoEstudiante='$estado' WHERE 
     codigoEstudiante='$codigo'");
 	 if(oci_execute($agregar)){
--- obtenerDocentes.php
+++ obtenerDocentes.php
@@ -1,8 +1,7 @@
 <?PHP
 include 'conexion.php';
-
-$iddocente=$_POST["iddocente"]
 $json=array();
+$iddocente=$_REQUEST["iddocente"];
 
 $query = "SELECT iddocente, nombredocente, tipodocente_idtipodocente, estadodocente 
 FROM docente WHERE iddocente='$iddocente'";
@@ -16,7 +15,7 @@ while ($row = oci_fetch_array ($statement, (OCI_NUM+OCI_RETURN_LOBS))) {
     $result["nombre"]= $row[1];
     $result["tipo"]= $row[2];
     $result["estado"]= $row[3];
-	$json['estudiantes'][]=$result;
+	$json['docentes'][]=$result;
 }
 echo json_encode($json)
 ?>
