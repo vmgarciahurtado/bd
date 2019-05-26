4bb6befaaa57b34ed30415443f994cec6e691bfb
vmgarciahurtado <vmgarciahurtado@gmail.com>
2019-05-26 18:59:04 -0500

    hola

--- consultaProgramas.php
+++ consultaProgramas.php
@@ -3,13 +3,13 @@ include 'conexion.php';
 
 $id=$_REQUEST["id"];
 $json=array();
-$query = "SELECT nombreprograma FROM programaacademico WHERE facultad_codigofacultad ='$id'";
+$query = "SELECT nombreprograma,idprogramaacademico FROM programaacademico WHERE facultad_codigofacultad ='$id'";
 $statement = oci_parse ($conexion, $query);
 oci_execute ($statement);
 
-while ($row = oci_fetch_array ($statement, (OCI_NUM+OCI_RETURN_LOBS))) {
-	//echo $row[0]."<br>";
+while ($row = oci_fetch_array ($statement, (OCI_NUM+OCI_RETURN_LOBS))) 
 	$result["programa"]= $row[0];
+	$result["id"]= $row[1];
 	$json['programa'][]=$result;
 }
 echo json_encode($json)
