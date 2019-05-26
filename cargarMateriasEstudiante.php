3d1ead6d3ec7e207faa53d2ba5df38d1d0087647
JuanM015 <JuanM015.juanmontoya015@gmail.com>
2019-05-26 18:58:54 -0500

    Realizaron PHP

--- cargarMateriasEstudiante.php
+++ cargarMateriasEstudiante.php
@@ -14,7 +14,7 @@ oci_execute ($statement);
 
 while ($row = oci_fetch_array ($statement, (OCI_NUM+OCI_RETURN_LOBS))) {
     $result["idmateria"]= $row[0];
-    $result["nombremateria"]= $row[0];
+    $result["nombremateria"]= $row[1];
 	$json['materias'][]=$result;
 }
 echo json_encode($json)
