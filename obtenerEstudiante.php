07d6534f8a9e1db9c2000f3fab89dff279774405
vmgarciahurtado <vmgarciahurtado@gmail.com>
2019-05-26 14:36:53 -0500

    cambios

--- obtenerEstudiante.php
+++ obtenerEstudiante.php
@@ -1,12 +1,12 @@
 <?PHP
 include 'conexion.php';
 
-$codigoestudiante=$_REQUEST["codigoestudiante"];
+$codigoestudiante=$_REQUEST["codigo"];
 $json=array();
 
-$query = "SELECT codigoEstudiante, cedulaestudiante, nombreestudiante, fechanacimiento, estadoestudiante, 
-direccionestudiante, telefonoestudiante, correoelectronico
-FROM estudiante WHERE codigoEstudiante='$codigoestudiante'";
+$query = "SELECT e.codigoEstudiante, e.cedulaestudiante, e.nombreestudiante, e.fechanacimiento, 
+e.direccionestudiante, e.telefonoestudiante, e.correoelectronico,p.nombreprograma,e.semestre_numerosemestre
+FROM estudiante e join programaacademico p on(e.pacademico_idpacademico = p.idprogramaacademico) WHERE codigoEstudiante='$codigoestudiante'";
 
 $statement = oci_parse ($conexion, $query);
 oci_execute ($statement);
@@ -17,12 +17,12 @@ while ($row = oci_fetch_array ($statement, (OCI_NUM+OCI_RETURN_LOBS))) {
     $result["cedula"]= $row[1];
     $result["nombre"]= $row[2];
     $result["fecha"]= $row[3];
-    $result["estado"]= $row[4];
-    $result["direccion"]= $row[5];
-    $result["telefono"]= $row[6];
-    $result["correo"]= $row[7];
-    //$result["programa"]= $row[8];
-    //$result["semestre"]= $row[9];
+
+    $result["direccion"]= $row[4];
+    $result["telefono"]= $row[5];
+    $result["correo"]= $row[6];
+    $result["programa"]= $row[7];
+    $result["semestre"]= $row[8];
 	$json['estudiantes'][]=$result;
 }
 echo json_encode($json)
