2d614ffb126c32cbfd76e8d287fa005989cf75c5
JuanM015 <JuanM015.juanmontoya015@gmail.com>
2019-05-26 13:25:27 -0500

    PHP

--- modificarDocente.php
+++ modificarDocente.php
@@ -3,14 +3,12 @@ include 'conexion.php';
 
 	$iddocente= $_POST["iddocente"];
 	$nombredocente= $_POST["nombredocente"];
-	$tipodocente= $_POST["tipodocente_idtipodocente"];
-	$estadoDocente=$_POST["estadodocente"];
     
-	$agregar = oci_parse($conexion,"UPDATE docente SET nombredocente='$nombredocente',tipodocente_idtipodocente = '$tipodocente', estadodocente='$estadoDocente' WHERE iddocente = '$iddocente'");
+	$agregar = oci_parse($conexion,"UPDATE docente SET nombredocente='$nombredocente' WHERE iddocente = '$iddocente'");
         
 	 if(oci_execute($agregar)){
-		echo "registra";
+		echo "modifico";
 	}else{
-		echo "noRegistra";
+		echo "noModifico";
 	} 
 ?>
