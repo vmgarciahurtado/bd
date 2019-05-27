43ad205583cf065dee084f721f288fa023a53ac7
vmgarciahurtado <vmgarciahurtado@gmail.com>
2019-05-27 09:22:13 -0500

    no sirve

--- create_pdf.php
+++ create_pdf.php
@@ -9,7 +9,6 @@ if(isset($_POST["hidden_html"]) && $_POST["hidden_html"] != '')
 {
  $file_name = 'GraficaPrueba.pdf';
  $html = '<link rel="stylesheet" href="bootstrap.min.css
- 
  <link rel="stylesheet" type="text/css" media="screen" href="estilos.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
     
