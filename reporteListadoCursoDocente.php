ae438070a3fc755cf767a19d6c593469d18059d5
vmgarciahurtado <vmgarciahurtado@gmail.com>
2019-05-27 09:22:04 -0500

    no sirve

--- reporteListadoCursoDocente.php
+++ reporteListadoCursoDocente.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>LISTADO CURSO direccionestudiante</title>
+    <script type="text/javascript" src="html2canvas.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="estilos.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
