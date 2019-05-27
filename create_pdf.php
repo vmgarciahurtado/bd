2b35bfb402a6ff11411c28b6b87120a5a3580109
vmgarciahurtado <vmgarciahurtado@gmail.com>
2019-05-27 08:59:50 -0500

    cambios

--- create_pdf.php
+++ create_pdf.php
@@ -8,13 +8,18 @@ include('pdf.php');
 if(isset($_POST["hidden_html"]) && $_POST["hidden_html"] != '')
 {
  $file_name = 'GraficaPrueba.pdf';
- $html = '<link rel="stylesheet" href="bootstrap.min.css">';
+ $html = '<link rel="stylesheet" href="bootstrap.min.css
+ <link rel="stylesheet" type="text/css" media="screen" href="estilos.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+    
+
+    ">';
  $html .= $_POST["hidden_html"];
 
  $pdf = new Pdf();
  $pdf->load_html($html);
  $pdf->render();
- $pdf->stream($file_name, array("Attachment" => false));
+ $pdf->stream($file_name, array("Attachment" => true));
 }
 
 ?>
